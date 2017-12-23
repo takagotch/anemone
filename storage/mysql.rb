4afb2bbe45072f211487f9b6d9ec565d1fe33be7
takagotch <dyaccb@gmail.com>
2017-12-24 07:11:37 +0900

    #create_schema

--- storage/mysql.rb
+++ storage/mysql.rb
@@ -17,3 +17,18 @@ def initialize(opts = {})
   create_schema
 end
 
+private
+
+def create_schema
+  @db.query <<SQL
+    create table if not exists anemone_storage (
+      id int(11) NOT NULL auto_increment,
+      page_key varchar(255),
+      page_data BLOB,
+      PRIMARY KEY (id),
+      key (page_key)
+    ) DEFAULT CHARSET=utf8;
+    SQL
+end
+
+
