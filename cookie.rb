8be564277722f1e2c19b83e9b1bbaf63ea448a27
takagotch <dyaccb@gmail.com>
2017-12-24 01:11:15 +0900

    cookies ={ :_auid => xxx}

---
+++ cookie.rb
@@ -0,0 +1,31 @@
+require 'anemone'
+require 'nokogiri'
+require 'kconv'
+
+urls = []
+urls.push("http://mixi.jp/home.pl?from=h_logo")
+
+cookies = {
+  :_auid    => "xxxxxxxxxx",
+  :emid     => "yyyyyyyyyy"
+  :session  => "sssssss",
+  :stamp    => "iiiiiiiiiii",
+  :vntgsync => "1",
+}
+opts    = {
+  :delay          => 1,
+  :accept_cookies => true,
+  :cookies        => cookies,
+  :depth_limit    => 0
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    doc = Nokogiri::HTML.parse(page.body.toutf8)
+    communityList = doc.xpath("//*[@id='communityList']//span/a")
+    communityList.each {|community|
+      puts community.content
+    }
+  end
+end
+
