6c45b9a574c7aefe3cdb1542a9d78b994794d08f
takagotch <dyaccb@gmail.com>
2017-12-24 07:31:56 +0900

    debug puts|p|pp

---
+++ debug_p.rb
@@ -0,0 +1,10 @@
+puts "string"
+p    "string"
+
+puts 15
+p    15
+
+puts "string\nstring"
+p    "string\nstring"
+
+
---
+++ debug_pp.rb
@@ -0,0 +1,6 @@
+require 'pp'
+ary = Array.new(6) { {:foo => :bar} }
+
+p  ary
+pp ary
+
