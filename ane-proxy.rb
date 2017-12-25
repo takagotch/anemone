ad9d92714d39205058b09c4e384fe21ed89dc3bf
takagotch <dyaccb@gmail.com>
2017-12-25 12:33:55 +0900

    cocproxy

---
+++ ane-proxy.rb
@@ -0,0 +1,18 @@
+require 'anemone'
+
+urls = []
+urls.push("http://www.yahoo.co.jp")
+
+opts = {
+  :proxy_host      => 'localhost',
+  :proxy_port      => '8080',
+  :obey_robots_txt => true,
+  :depth_limit     => 0
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    puts page.doc.xpath("//title/text()").to_s if page.doc
+  end
+end
+
