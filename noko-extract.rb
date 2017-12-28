cbb4113aa1ef0aecfaaf45e5a8a1e1dd88abc022
takagotch <dyaccb@gmail.com>
2017-12-29 05:14:13 +0900

    nokogiri-extract text

--- noko-extract.rb
+++ noko-extract.rb
@@ -1,7 +1,8 @@
 require 'open-uri'
 require 'nokogiri'
 
-html = open().read
-doc = Nokogiri::HTML()
-puts doc.xpath().text
+html = open(
+  'http"//www.bloger.com/entry/20171229/xxxxxxxxxx').read
+doc = Nokogiri::HTML(html)
+puts doc.xpath("//div[@class='entry-content']").text
 
