86518bc247182456869af42d4156dcebb84fe7b0
takagotch <dyaccb@gmail.com>
2017-12-29 05:15:38 +0900

    => content, text

--- extract.rb
+++ extract.rb
@@ -1,7 +1,9 @@
 require 'extractcontent'
 require 'open-uri'
 
-html = open().read
+html = open(
+	'http://www.bloger.com/entry/20171229/xxxxxxxx').read
+content, title = ExtractContent.analyse(html)
 puts title
 puts content
 
